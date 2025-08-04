a29ed63a1975e4f7d81cca32816a2109c38e1530
L3odr0id <leon.leonc@yandex.ru>
2025-08-04 11:51:58 +0000

    [ fix ] Header path

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,13 +2,14 @@
 	import { GithubSolid } from 'flowbite-svelte-icons';
 	import { Navbar, NavBrand, A } from 'flowbite-svelte';
 	import { githubUrl } from '$lib/consts';
+	import { base } from '$app/paths';
 </script>
 
 <header>
 	<Navbar
 		class="fixed top-0 z-40 w-full border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800"
 	>
-		<NavBrand href="/">
+		<NavBrand href="{base}/">
 			<img src="/logo_no_bg.png" class="mr-2 h-8 sm:h-9" alt="Project Logo" />
 			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
 				>Verilog model</span
