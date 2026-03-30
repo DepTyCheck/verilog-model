54940c6b81c6197d97b24882234cb2bd2bc01dcb
L3odr0id <leodroid54@gmail.com>
2026-03-30 21:31:15 +0300

    [ ci ] Print errors stats comparison

---
+++ ci/runner/combined_report/__init__.py
@@ -0,0 +1,5 @@
+from .previous_report import PreviousReport
+from .tools_report_list import ToolsReportsList
+from .combined_report import CombinedReport
+from .result_report import ResultReport
+from .percentages import total_test_count, occurrence_pct
